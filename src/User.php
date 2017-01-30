3dd1c2c239ce1f07bbb5c711bb798550c78ced9f
Kerem Güneş <k-gun@mail.com>
2017-01-31 02:16:28 +0300

    Add Froq\Acl\User object.

--- src/User.php
+++ src/User.php
@@ -10,55 +10,104 @@ final class User
     private $role;
     private $permissions = [];
 
-    final public function __construct()
-    {}
+    final public function __construct(array $info = null)
+    {
+        if ($info) {
+            isset($info['id'])   && $this->setId($info['id']);
+            isset($info['name']) && $this->setName($info['name']);
+            isset($info['role']) && $this->setRole($info['role']);
+        }
+    }
 
-    // final public function setId($id) {}
-    // final public function getId() {}
+    final public function setId($id)
+    {
+        $this->id = $id;
+    }
+    final public function getId()
+    {
+        return $this->id;
+    }
 
-    // final public function setName($name) {}
-    // final public function getName() {}
+    final public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+    final public function getName()
+    {
+        return $this->name;
+    }
 
-    // final public function setRole(Role $role) {}
-    // final public function getRole() {}
+    final public function setRole(string $role)
+    {
+        $this->role = $role;
+    }
+    final public function getRole()
+    {
+        return $this->role;
+    }
 
-    // final public function setPermissions(array $permissions) {}
-    // final public function getPermissions(): array {}
+    final public function setPermission($uri, array $permission)
+    {
+        $this->permissions[$uri] = $permission;
+    }
+    final public function getPermission($uri)
+    {
+        return $this->permissions[$uri] ?? null;
+    }
+
+    final public function setPermissions(array $permissions)
+    {
+        $this->permissions = $permissions;
+    }
+    final public function getPermissions(): array
+    {
+        return $this->permissions;
+    }
 
     final public function isLoggedIn(): bool
     {
-        return (null !== $this->id);
+        return ($this->id !== null);
     }
 
-    final public function hasAccessTo(Resource $resource): bool
+    final public function hasAccessTo(string $uri): bool
     {
-        return $this->role->hasAccessTo($resource);
+        return !!$this->getPermission($uri);
     }
 
-    // Alice: read,write; Bob: read
-    final public function canRead(Resource $resource): bool
-    {
-        return $this->role->canRead($resource);
-    }
-    final public function canWrite(Resource $resource): bool
-    {
-        return $this->role->canWrite($resource);
-    }
+    // // Alice: read,write; Bob: read
+    // final public function canRead(Resource $resource): bool
+    // {
+    //     return $this->role->canRead($resource);
+    //
+    //     $permissions = $this->getPermission($uri);
+        // if ($permissions) {
+        //     foreach ($permissions as $permission) {
+        //         if ($permission == Acl::RULE_ALL || $permission == Acl::RULE_READ) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+    // }
+    // final public function canWrite(Resource $resource): bool
+    // {
+    //     return $this->role->canWrite($resource);
+    // }
 
-    final public function info(bool $full = false)
-    {
-        if (null === $this->id && null === $this->name) {
-            return null;
-        }
+    // final public function info(bool $full = false)
+    // {
+    //     if ($this->id === null && $this->name === null) {
+    //         return null;
+    //     }
 
-        $return = sprintf('id=%s(%s)', $this->id, $this->name);
-        if ($full) {
-            foreach ($this->role->getResources() as $resource) {
-                $return .= sprintf(' resource(%s:%s)',
-                    $resource->getName(), join(',', $resource->getPermissions()));
-            }
-        }
+    //     $return = sprintf('id=%s(%s)', $this->id, $this->name);
+    //     if ($full) {
+    //         foreach ($this->role->getResources() as $resource) {
+    //             $return .= sprintf("\nresource(%s:%s)",
+    //                 $resource->getName(), join(',', $resource->getPermissions()));
+    //         }
+    //     }
 
-        return $return;
-    }
+    //     return $return;
+    // }
 }
